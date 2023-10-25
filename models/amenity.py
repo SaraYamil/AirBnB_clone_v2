85201084db7d3ea028131067275fef6af39541d9
SARA YAMIL <sarayamil18@gmail.com>
2023-10-25 10:21:58 -0700

    sara

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Amennity class
+"""
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity class
+
+    Attributes:
+        name (str): name of the amenity
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
--- models/review.py
+++ models/review.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""
+Review Module
+"""
+
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """
+    Review class
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews
+        place_id (sqlalchemy String): The Review's place ID
+        user_id (sqlalchemy String): The Review's user ID
+        text (sqlalchemy String): The Review's text
+
+    Methods:
+        __init__(self, *args, **kwargs): Initializes a Review instance
+    """
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
