4963cfdd42a2fcdf7ce62233e20398742237fccd
SARA YAMIL <sarayamil18@gmail.com>
2023-10-25 10:08:31 -0700

    sara

--- models/city.py
+++ models/city.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""
+City class
+"""
+
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+from models.base_model import BaseModel, Base
+
+
+class City(BaseModel, Base):
+    """
+    City class for AirBnB clone project
+
+    Attributes:
+        name (str): name of city
+        state_id (str): state id
+    """
+
+    __tablename__ = "cities"
+    name = (
+        Column(String(128), nullable=False)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    state_id = (
+        Column(String(60), ForeignKey("states.id"), nullable=False)
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else ""
+    )
+    places = (
+        relationship("Place", cascade="all, delete, delete-orphan",
+                     backref="cities")
+        if os.getenv("HBNB_TYPE_STORAGE") == "db"
+        else None
+    )
--- models/user.py
+++ models/user.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+User Module
+"""
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """
+    User class
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Users
+        email (sqlalchemy String): The user's email address
+        password (sqlalchemy String): The user's password
+        first_name (sqlalchemy String): The user's first name
+        last_name (sqlalchemy String): The user's last name
+        places (sqlalchemy relationship): The User-Place relationship
+        reviews (sqlalchemy relationship): The User-Review relationship
+
+    Methods:
+        __init__(self, *args, **kwargs): Initializes a User instance
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
